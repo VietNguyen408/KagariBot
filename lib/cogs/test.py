e763cb9fd4ed8bd3a602482acc77ca512c27f602
mckenzie <inzector0102@gmail.com>
2020-12-18 20:03:58 +0200

    Add slap command

--- lib/cogs/test.py
+++ lib/cogs/test.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+from discord import Member
 from discord.ext.commands import Cog
 from discord.ext.commands import command
 
@@ -9,6 +12,21 @@ class Test(Cog):
     @command(name='ping', aliases=['p'])
     async def hello_world(self, ctx):
         await ctx.send("pong!")
+
+    @command(name='start')
+    async def start(self, ctx):
+        guild = ctx.guild
+        author = ctx.message.author
+        await ctx.send(f'Hello {author.mention}!')
+        if not ctx.message.author.voice:
+            await ctx.send('You have to be in a voice channel to start a game!')
+            return
+        game_channel = ctx.message.author.voice.channel
+        await game_channel.connect()
+
+    @command(name='slap', aliases=['hit'])
+    async def bitch_slap(self, ctx, member: Member, *, reason: Optional[str] = 'no reason'):
+        await ctx.send(f'{ctx.author.display_name} slapped {member.display_name} for {reason}!')
 
     @Cog.listener()
     async def on_ready(self):
